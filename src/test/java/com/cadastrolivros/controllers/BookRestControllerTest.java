bc4958e41de596ec174ab53f0dcbd1a508b7846c
Ana Beatriz Trindade <anabeatrizdmt@hotmail.com>
2023-05-06 18:09:34 -0300

    feat: adding unit tests structure

---
+++ src/test/java/com/cadastrolivros/controllers/BookRestControllerTest.java
@@ -0,0 +1,20 @@
+package com.cadastrolivros.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookRestControllerTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
---
+++ src/test/java/com/cadastrolivros/services/BookServiceTest.java
@@ -0,0 +1,24 @@
+package com.cadastrolivros.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookServiceTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
